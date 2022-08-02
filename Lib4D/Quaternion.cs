fdd7ccd454c0398a1518d3fda76d28c0bb3acc83
Evhen Diulin <ydiulin@rework-space.com>
2022-08-02 23:54:54 +0300

    Added Quaternion, operators + and -

---
+++ Lib4D/Quaternion.cs
@@ -0,0 +1,88 @@
+﻿namespace Lib4D
+{
+	public struct Quaternion
+	{
+		public Complex ri, jk;
+		#region Getters
+		public double R
+		{
+			get => ri.r;
+			set => ri.r = value;
+		}
+		public double I
+		{
+			get => ri.i;
+			set => ri.i = value;
+		}
+		public double J
+		{
+			get => jk.r;
+			set => jk.r = value;
+		}
+		public double K
+		{
+			get => jk.i;
+			set => jk.i = value;
+		}
+		#endregion
+
+		#region Constructors
+		public Quaternion(double r)
+		{
+			this.ri = new Complex(r, 0);
+			this.jk = new Complex();
+		}
+
+		public Quaternion(double r, double i)
+		{
+			this.ri = new Complex(r, i);
+			this.jk = new Complex();
+		}
+		public Quaternion(double r, double i, double j)
+		{
+			this.ri = new Complex(r, i);
+			this.jk = new Complex(j, 0);
+		}
+		public Quaternion(double r, double i, double j, double k)
+		{
+			this.ri = new Complex(r, i);
+			this.jk = new Complex(j, k);
+		}
+
+		public Quaternion(Complex ri)
+		{
+			this.ri = ri;
+			this.jk = new Complex();
+		}
+		public Quaternion(Complex ri, Complex jk)
+		{
+			this.ri = ri;
+			this.jk = jk;
+		}
+		#endregion
+
+		#region Math Operators
+		public static Quaternion operator +(Quaternion a, Quaternion b)
+		{
+			return new Quaternion(a.ri + b.ri, a.jk + b.jk);
+		}
+
+		public static Quaternion operator -(Quaternion a, Quaternion b)
+		{
+			return new Quaternion(a.ri - b.ri, a.jk - b.jk);
+		}
+		#endregion
+
+		#region Comparisons
+		public static bool operator ==(Quaternion a, Quaternion b)
+		{
+			return a.ri == b.ri && a.jk == b.jk;
+		}
+
+		public static bool operator !=(Quaternion a, Quaternion b)
+		{
+			return a.ri != b.ri || a.jk != b.jk;
+		}
+		#endregion
+	}
+}
---
+++ Lib4D_Tests/QuaternionUnitTest.cs
@@ -0,0 +1,86 @@
+﻿using Lib4D;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Lib4D_Tests
+{
+	[TestClass]
+	public class QuaternionUnitTest
+	{
+		private Random _rnd = new Random();
+
+		[TestMethod]
+		public void Equals()
+		{
+			Quaternion a = new Quaternion();
+			Quaternion b = new Quaternion(0, 0, 0, 0);
+			Assert.IsTrue(a == b);
+
+			a = new Quaternion(-1, 2, 3, -4);
+			b = new Quaternion(-1, 2, 3, -4);
+			Assert.IsTrue(a == b);
+
+			a = new Quaternion(-3, 2, 3, -4);
+			b = new Quaternion(-1, 2, 3, -4);
+			Assert.IsFalse(a == b);
+
+			a = new Quaternion(-1, 2, 3, -4);
+			b = new Quaternion(-1, 3, 3, -4);
+			Assert.IsFalse(a == b);
+
+			a = new Quaternion(-1, 2, 3, -4);
+			b = new Quaternion(-1, 2, -3, -4);
+			Assert.IsFalse(a == b);
+
+			a = new Quaternion(-1, 2, 3, -5);
+			b = new Quaternion(-1, 2, 3, -4);
+			Assert.IsFalse(a == b);
+		}
+
+		[TestMethod]
+		public void Add()
+		{
+			Quaternion a = new Quaternion(0, 0);
+			Quaternion b = new Quaternion(1, 1);
+			Assert.AreEqual(a + b, b);
+
+			a = new Quaternion(1, 0, 0, 0);
+			Assert.AreEqual(a + a, new Quaternion(2, 0, 0, 0));
+			a = new Quaternion(0, 1, 0, 0);
+			Assert.AreEqual(a + a, new Quaternion(0, 2, 0, 0));
+			a = new Quaternion(0, 0, 1, 0);
+			Assert.AreEqual(a + a, new Quaternion(0, 0, 2, 0));
+			a = new Quaternion(0, 0, 0, 1);
+			Assert.AreEqual(a + a, new Quaternion(0, 0, 0, 2));
+
+			a = new Quaternion(-3, 2, -1, 4);
+			b = new Quaternion(10, 1, -5, -1);
+			Quaternion c = new Quaternion(7, 3, -6, 3);
+			Assert.AreEqual(a + b, c);
+		}
+
+		[TestMethod]
+		public void Sub()
+		{
+			Quaternion zero = new Quaternion(0, 0, 0, 0);
+			Quaternion ones = new Quaternion(1, 1, 1, 1);
+			Assert.AreEqual(ones - zero, ones);
+			Assert.AreEqual(ones - ones, zero);
+
+			for (int i = 0; i < 10; i++)
+			{
+				Quaternion a = getQuaternion();
+				Quaternion b = getQuaternion();
+				Quaternion c = a + b;
+
+				Assert.AreEqual(c - a, b);
+				Assert.AreEqual(c - b, a);
+			}
+		}
+
+		private Quaternion getQuaternion()
+		{
+			return new Quaternion(_rnd.Next(200) - 100, _rnd.Next(200) - 100);
+		}
+	}
+}
