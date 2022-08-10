97fd0cbf98bf6f4189e64ef718a8c56e7aed238a
Evhen Diulin <ydiulin@rework-space.com>
2022-08-10 22:56:04 +0300

    Added MatrixMath and first operator Transpose()

---
+++ Lib4D/MatrixMath.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Lib4D
+{
+	public static class MatrixMath {
+		public static double[,] Transpose (this double[,] matrixA)
+		{
+			int width = matrixA.GetLength(0);
+			int height = matrixA.GetLength(1);
+			double[,] res = new double[height, width];
+
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					res[y, x] = matrixA[x, y];
+				}
+			}
+
+			return res;
+		}
+
+		public static bool EqualsTo(this double[,] a, double[,] b)
+		{
+			if (a.GetLength(0) != b.GetLength(0) || a.GetLength(1) != b.GetLength(1))
+			{
+				return false;
+			}
+
+			int width = a.GetLength(0);
+			int height = a.GetLength(1);
+
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					if (a[x, y] != b[x, y])
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		public static double[,] Mul (this double[,] a, double[,] b)
+		{
+			// Ширина матрицы A равна высоте B
+			if (a.GetWidth() != b.GetHeight()) {
+				throw new Exception("Cannot execute multiplication of matrixes, because their sizes are not suitable");
+			}
+
+			int commonWidth = a.GetWidth();
+			int width = b.GetWidth();
+			int height = a.GetHeight();
+			double[,] c = new double[width, height];
+
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					double sum = 0.0;
+					for (int i = 0; i < commonWidth; i++)
+					{
+						sum += a[i, y] * b[x, i];
+					}
+					c[x, y] = sum;
+				}
+			}
+
+			return c;
+		}
+
+		public static int GetWidth (this double[,] a)
+		{
+			return a.GetLength(0);
+		}
+
+		public static int GetHeight(this double[,] a)
+		{
+			return a.GetLength(1);
+		}
+	}
+}
---
+++ Lib4D_Tests/MatrixMathUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using Lib4D;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Lib4D_Tests
+{
+	[TestClass]
+	public class MatrixMathUnitTest
+	{
+		private Random _rnd = new Random();
+
+		[TestMethod]
+		public void Transpose()
+		{
+			double[,] m = new double[3, 3]
+			{
+				{  0,  2, 73 },
+				{ -1,  0,  2 },
+				{ -2, -1,  1 }
+			};
+
+			double[,] m2 = new double[3, 3]
+			{
+				{  0, -1, -2 },
+				{  2,  0, -1 },
+				{ 73,  2,  1 }
+			};
+
+			AssertEqual(m2, m.Transpose());
+		}
+
+		private void AssertEqual(double[,] a, double[,] b)
+		{
+			try
+			{
+				Assert.IsTrue(a.EqualsTo(b));
+			}
+			catch
+			{
+				Assert.AreEqual(a, b);
+			}
+		}
+	}
+}
