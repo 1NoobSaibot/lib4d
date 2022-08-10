5550764b503781edaededf745f99c44fee44eaa6
Evhen Diulin <ydiulin@rework-space.com>
2022-08-10 23:40:17 +0300

    Simple test for MatrixMath.Mul()

--- Lib4D_Tests/MatrixMathUnitTest.cs
+++ Lib4D_Tests/MatrixMathUnitTest.cs
@@ -29,6 +29,35 @@ namespace Lib4D_Tests
 			AssertEqual(m2, m.Transpose());
 		}
 
+		[TestMethod]
+		public void Mul()
+		{
+			double[,] identity = new double[1, 1] { { 1 } };
+			double[,] three = new double[1, 1] { { 3 } };
+			double[,] five = new double[1, 1] { { 5 } };
+			double[,] fifteen = new double[1, 1] { { 15 } };
+
+			AssertEqual(three, MatrixMath.Mul(identity, three));
+			AssertEqual(fifteen, MatrixMath.Mul(three, five));
+
+			double[,] a = new double[2, 1] { { 1 }, { 1 } };
+			double[,] b = new double[1, 2] { { 1 , 1 } };
+
+			AssertEqual(
+				new double[1, 1] { { 2 } },
+				MatrixMath.Mul(a, b)
+			);
+
+			AssertEqual(
+				new double[2, 2]
+				{
+					{ 1, 1 },
+					{ 1, 1 }
+				},
+				MatrixMath.Mul(b, a)
+			);
+		}
+
 		private void AssertEqual(double[,] a, double[,] b)
 		{
 			try
