7f593a81c92b02f6d0009922d89a61c2edc23cbc
Evhen Diulin <ydiulin@rework-space.com>
2022-08-16 20:23:03 +0300

    Added unsafe indexer for Vector4D and it can be iterable:)

---
+++ Lib4D_Tests/Vectors/Vector4D.spec.cs
@@ -0,0 +1,48 @@
+﻿using Lib4D;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Lib4D_Tests.Vectors
+{
+	[TestClass]
+	public class Vector4DSpec
+	{
+		[TestMethod]
+		public void GettingComponentsByIndexer()
+		{
+			Vector4D v = new Vector4D(3, 5, 7, 11);
+			Assert.AreEqual(3, v[0]);
+			Assert.AreEqual(5, v[1]);
+			Assert.AreEqual(7, v[2]);
+			Assert.AreEqual(11, v[3]);
+
+			Assert.AreEqual(v.X, v[0]);
+			Assert.AreEqual(v.Y, v[1]);
+			Assert.AreEqual(v.Z, v[2]);
+			Assert.AreEqual(v.Q, v[3]);
+		}
+
+
+		[TestMethod]
+		public void SettingcomponentsByIndexer()
+		{
+			Vector4D v = new Vector4D();
+			v[0] = 1;
+			v[1] = 2;
+			v[2] = 3;
+			v[3] = 4;
+			Assert.AreEqual(new Vector4D(1, 2, 3, 4), v);
+		}
+
+
+		[TestMethod]
+		public void IndexerThrowsErrorsWhenIndexIsOutOfRange()
+		{
+			Vector4D v = new Vector4D();
+			Assert.ThrowsException<Exception>(() => v[-1] = 4);
+			Assert.ThrowsException<Exception>(() => v[4] = 4);
+			Assert.ThrowsException<Exception>(() => { double d = v[-1]; });
+			Assert.ThrowsException<Exception>(() => { double d = v[4]; });
+		}
+	}
+}
