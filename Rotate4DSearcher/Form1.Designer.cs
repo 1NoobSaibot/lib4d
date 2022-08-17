94d9024267ab25400872db833d96e711c6a790cf
Evhen Diulin <ydiulin@rework-space.com>
2022-08-17 21:21:44 +0300

    Init of Rotation4DSearcher function

---
+++ Rotate4DSearcher/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Rotate4DSearcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.loadButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(378, 426);
+			this.textBox1.TabIndex = 0;
+			// 
+			// loadButton
+			// 
+			this.loadButton.Location = new System.Drawing.Point(713, 13);
+			this.loadButton.Name = "loadButton";
+			this.loadButton.Size = new System.Drawing.Size(75, 23);
+			this.loadButton.TabIndex = 1;
+			this.loadButton.Text = "Load Samples";
+			this.loadButton.UseVisualStyleBackColor = true;
+			this.loadButton.Click += new System.EventHandler(this.onLoad);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.loadButton);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button loadButton;
+	}
+}
+
---
+++ Rotate4DSearcher/SamplesStore.cs
@@ -0,0 +1,93 @@
+﻿using Lib4D;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Rotate4DSearcher
+{
+	internal static class SamplesStore
+	{
+		private const string fileName = "Samples.json";
+		public static List<Sample> Samples => GetSamples();
+		private static List<Sample> _samples;
+		public static event EventHandler<List<Sample>> SamplesChanged;
+
+
+		public static List<Sample> GetSamples()
+		{
+			if (_samples == null)
+			{
+				_Load();
+			}
+
+			return _samples;
+		}
+
+
+		public static void AddNewSample(Bivector4D rotationSurface)
+		{
+			_ThrowIfExists(rotationSurface);
+
+			_samples.Add(new Sample() { rotationSurface = rotationSurface });
+			_Save();
+
+			SamplesChanged(_samples, null);
+		}
+
+
+		private static void _ThrowIfExists(Bivector4D surface)
+		{
+			for (int i = 0; i < _samples.Count; i++)
+			{
+				if (surface == _samples[i].rotationSurface)
+				{
+					throw new Exception("This surface already exists");
+				}
+			}
+		}
+
+
+		private static void _Load()
+		{
+			if (!File.Exists(fileName))
+			{
+				_samples = new List<Sample>();
+				_Save();
+				return;
+			}
+
+			using (StreamReader reader = new StreamReader(fileName))
+			{
+				string json = reader.ReadToEnd();
+				_samples = JsonConvert.DeserializeObject<List<Sample>>(json);
+			}
+		}
+
+
+		private static void _Save()
+		{
+			using (StreamWriter writer = new StreamWriter(fileName, false))
+			{
+				string json = JsonConvert.SerializeObject(_samples);
+				writer.Write(json);
+			}
+		}
+	}
+
+
+	[Serializable]
+	internal class Sample
+	{
+		public Bivector4D rotationSurface;
+		public List<QuestionAnswerPair> pairs = new List<QuestionAnswerPair>();
+	}
+
+
+	[Serializable]
+	internal class QuestionAnswerPair
+	{
+		public Vector4D question;
+		public Vector4D answer;
+	}
+}
